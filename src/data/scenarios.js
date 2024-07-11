f9645dc7afca0868b81ba35bef1f9e993af54e4a
sashuttle <sashuttle25@outlook.com>
2024-07-11 10:56:30 -0400

    added another ending and expanded the dice jail path

--- src/data/scenarios.js
+++ src/data/scenarios.js
@@ -9,7 +9,7 @@ export const scenarios = [
             {text:'Your the only "1" I need', emotion:'blush', nextScenario: 4},
         ],
     },
-    // The Happy Path 
+    // The Happy Path  25 so far
     {
         id: 2,
         text:'I knew you would understand! Let\'s get back to playing!',
@@ -19,7 +19,7 @@ export const scenarios = [
             {text:'Of course, anything for you!', emotion:'blush'},
         ],
     },
-    //The Jail Path
+    //The Jail Path 207 words total for this path
     {
         id: 3,
         text:'WHAT! THATS JUST HOW PROBABILITY WORKS!',
@@ -29,7 +29,42 @@ export const scenarios = [
             {text:'You\re still my favorite dice!', emotion:'blush'},
         ],
     },
-    //The Lovers Path 147 words 
+    {
+        id: 10,
+        text:'I\m sorry, but you have to admit, that fireball was really cool!',
+        responses: [
+            {text:'...yeah it was pretty cool, but you still rolled a nat 1!', emotion:'happy'},
+            {text:'MY EYEBROWS WILL TAKE FOREVER TO GROW BACK!', emotion:'sad'},
+            {text:'I WISH THAT FIREBALL WOULD HAVE MELTED YOU!', emotion:'angry'},
+        ],
+    },
+    {
+        id: 11,
+        text:'Okay well, remember that time we snuck past the guards at Cragmaw Castle?',
+        responses: [
+            {text:'You had me sing my own theme song...it was a catchy tune', emotion:'happy'},
+            {text:'I sneezed so loud all of the guards were alerted!!!!', emotion:'angry'},
+            {text:'I ended up tripping over my own feet!', emotion:'blush'},
+        ],
+    },
+    {
+        id: 12,
+        text:'Please don\t throw me in dice jail, remember the time we saw that sexy noble at the royal feast?!',
+        responses: [
+            {text:'They were very sexy, but your nat 1 didn\t help...', emotion:'happy'},
+            {text:'BECAUSE OF YOU THEY PUT A RESTRAINING ORDER ON ME! YOURE THE WORST WINGMAN EVER!!!!', emotion:'angry'},
+            {text:'Things were going great and I thought it was going to go somewhere until...I burped in their face', emotion:'blush'},
+        ],
+    },
+    {
+        id: 13,
+        text:'Look, I\m sorry for all the nat 1s. I promise it won\t happen again!!!',
+        responses: [
+            {text:'Fine, one last chance and thats it!', emotion:'happy'},
+            {text:'YOU ARE BY FAR THE WORST DICE EVER! I\M DONE WITH YOU!!!', emotion:'sad'},
+        ],
+    },
+    //The Lovers Path 147 words total for this path 
     {
         id: 4,
         text:'Oh... I had no idea that\'s how you felt...',
@@ -76,6 +111,13 @@ export const scenarios = [
         id: 9,
         ending: true,
         text:'You moan and release the dice onto the table even though the dungeon master did not ask. You turn and look at the table. Everyone is staring at you. There is silence. The player to your left yells “NAT 20 HELL YEAH” Everyone Cheers. You have no idea what you were suppose to be rolling for but it feels great. You look down at your die, happy you gave it another chance.',
+        responses: [
+        ],
+    },
+    {//65 words
+        id: 14,
+        ending: true,
+        text:'You\re seething with frustration and glare at your dice as it lands yet another dreaded nat 1. Your face reddens, hands trembling with anger as you snatch your traitorous dice from the table. With a dramatic flourish, you toss the dice into a small, ornate box labeled "Dice Jail", slamming the lid shut with finality. The other players echange amused glances, empathizing with your frustration.',
         responses: [
         ],
     },
